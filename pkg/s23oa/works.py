239fb0eaf0694a490fb064d7e143c0dd796d2df0
kareemabdelmaqsoud <kabdelma@andrew.cmu.edu>
2023-04-24 03:14:33 +0000

    added core files for the package

---
+++ pkg/s23oa/works.py
@@ -0,0 +1,187 @@
+import time
+import requests
+import bibtexparser
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
+
+class Works:
+    def __init__(self, oaid):
+        self.oaid = oaid
+        self.req = requests.get(f'https://api.openalex.org/works/{oaid}')
+        self.data = self.req.json()
+        
+    def __str__(self):
+        return 'str'
+        
+    def __repr__(self):
+        _authors = [au['author']['display_name'] for au in self.data['authorships']]
+        if len(_authors) == 1:
+            authors = _authors[0]
+        else:
+            authors = ', '.join(_authors[0:-1]) + ' and ' + _authors[-1]
+            
+        title = self.data['title']
+        
+        journal = self.data['host_venue']['display_name']
+        volume = self.data['biblio']['volume']
+        
+        issue = self.data['biblio']['issue']
+        if issue is None:
+            issue = ', '
+        else:
+            issue = ', ' + issue
+
+        pages = '-'.join([self.data['biblio'].get('first_page', '') or '',
+                          self.data['biblio'].get('last_page', '') or ''])
+        year = self.data['publication_year']
+        citedby = self.data['cited_by_count']
+        
+        oa = self.data['id']
+        s = f'{authors}, {title}, {volume}{issue}{pages}, ({year}), {self.data["doi"]}. cited by: {citedby}. {oa}'
+        return s
+    
+    def _repr_markdown_(self):
+        _authors = [f'[{au["author"]["display_name"]}]({au["author"]["id"]})' for au in self.data['authorships']]
+        if len(_authors) == 1:
+            authors = _authors[0]
+        else:
+            authors = ', '.join(_authors[0:-1]) + ' and ' + _authors[-1]
+            
+        title = self.data['title']
+        
+        journal = f"[{self.data['host_venue']['display_name']}]({self.data['host_venue']['id']})"
+        volume = self.data['biblio']['volume']
+        
+        issue = self.data['biblio']['issue']
+        if issue is None:
+            issue = ', '
+        else:
+            issue = ', ' + issue
+            
+        pages = '-'.join([self.data['biblio'].get('first_page', '') or '',
+                          self.data['biblio'].get('last_page', '') or ''])
+        year = self.data['publication_year']
+        citedby = self.data['cited_by_count']
+        
+        oa = self.data['id']
+        
+        # Citation counts by year
+        years = [e['year'] for e in self.data['counts_by_year']]
+        counts = [e['cited_by_count'] for e in self.data['counts_by_year']]
+    
+        fig, ax = plt.subplots()
+        ax.bar(years, counts)
+        ax.set_xlabel('year')
+        ax.set_ylabel('citation count')
+        data = print_figure(fig, 'png') # save figure in string
+        plt.close(fig)
+        
+        b64 = base64.b64encode(data).decode('utf8')
+        citefig = (f'![img](data:image/png;base64,{b64})')
+        
+        s = f'{authors}, *{title}*, **{journal}**, {volume}{issue}{pages}, ({year}), {self.data["doi"]}. cited by: {citedby}. [Open Alex]({oa})'
+        
+        s += '<br>' + citefig
+        return s
+    
+    @property
+    def ris(self):
+        fields = []
+        if self.data['type'] == 'journal-article':
+            fields += ['TY  - JOUR']
+        else:
+            raise Exception("Unsupported type {self.data['type']}")
+        
+        for author in self.data['authorships']:
+            fields += [f'AU  - {author["author"]["display_name"]}']
+            
+        fields += [f'PY  - {self.data["publication_year"]}']
+        fields += [f'TI  - {self.data["title"]}']
+        fields += [f'JO  - {self.data["host_venue"]["display_name"]}']
+        fields += [f'VL  - {self.data["biblio"]["volume"]}']
+        
+        if self.data['biblio']['issue']:
+            fields += [f'IS  - {self.data["biblio"]["issue"]}']
+        
+        
+        fields += [f'SP  - {self.data["biblio"]["first_page"]}']
+        fields += [f'EP  - {self.data["biblio"]["last_page"]}']
+        fields += [f'DO  - {self.data["doi"]}']
+        fields += ['ER  -']
+                
+        ris = '\n'.join(fields)
+        ris64 = base64.b64encode(ris.encode('utf-8')).decode('utf8')
+        uri = f'<pre>{ris}<pre><br><a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>'
+        from IPython.display import HTML
+        return HTML(uri)
+    
+    def related_works(self):
+        rworks = []
+        for rw_url in self.data['related_works']:
+            rw = Works(rw_url)
+            rworks += [rw]
+            time.sleep(0.101)
+        return rworks
+    
+    def citing_works(self):
+        citations_json = requests.get(self.data["cited_by_api_url"]).json()
+        citing_papers = citations_json["results"]
+        for i, paper in enumerate(citing_papers):
+            print(f'{i+1:2d}- Title: {paper["title"]}')
+            print("Doi:", paper["doi"])
+            print("Publication_year:", paper["publication_year"])
+            print("\n")
+            
+    def references(self):
+        for i, url in enumerate(self.data["referenced_works"]):
+            time.sleep(0.101)
+            paper_id = url[21:]
+            referenced_paper = requests.get(f"https://api.openalex.org/works/{paper_id}").json()
+            print(f'{i+1:2d}- Title:{referenced_paper["title"]}')
+            print("Doi:", referenced_paper["doi"])
+            print("Publication_year:", referenced_paper["publication_year"])
+            print("\n")
+    
+
+
+    def bibtex(self):
+        year = self.data['publication_year']
+        _authors = [au['author']['display_name'] for au in self.data['authorships']]
+        if len(_authors) == 1:
+            authors = _authors[0]
+        else:
+            authors = ', '.join(_authors[0:-1]) + ' and ' + _authors[-1]
+            
+            
+            
+        title = self.data['title']
+        
+        journal = self.data['host_venue']['display_name']
+        volume = self.data['biblio']['volume']
+        issue = self.data['biblio']['issue']
+        if issue is None:
+            issue = ', '
+        else:
+            issue = ', ' + issue
+
+        pages = '-'.join([self.data['biblio'].get('first_page', '') or '',
+                          self.data['biblio'].get('last_page', '') or ''])
+
+        
+        oa = self.data['id']
+        
+        db = BibDatabase()
+        db.entries =     [
+        {'journal': journal,
+         'pages': str(pages),
+         'title': title,
+         'year': str(year),
+         'volume': str(volume),
+         'ID': authors.split()[-1]+str(year),
+         'author': authors,
+         'doi': self.data["doi"],
+         'ENTRYTYPE': self.data['type']}]
+        writer = BibTexWriter()
+        writer.indent = '    '     # indent entries with 4 spaces instead of one
+        writer.comma_first = False  # place the comma at the beginning of the line
+        print(writer.write(db))
---
+++ pkg/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+"""A package for bibtex utilities."""
+
+from setuptools import setup
+
+setup(
+    name="s23oa",
+    version="0.0.1",
+    description="OpenAlex utilities",
+    maintainer="Kareem Abdelmaqsoud",
+    maintainer_email="kabdelma@andrew.cmu.edu",
+    license="MIT",
+    packages=["s23oa"],
+    scripts=[],
+    long_description="""A set of OpenAlex utilities""",
+)
